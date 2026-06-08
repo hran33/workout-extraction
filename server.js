5f46f36b3b9938e5edceacfa0f8e96be0ac0ed80
Ivy Hao <ranhao@Mac-5928.lan>
2026-06-07 21:27:50 -0400

    Add CORS headers to allow browser requests from Lovable

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ import { normalizeXiaohongshu } from './src/normalizeXiaohongshu.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 
 function downloadFile(url, destPath) {
   return new Promise((resolve, reject) => {
